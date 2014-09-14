8a4842906d8a26d5b9fa8c06f9d4ba7d9e4c4c1b
dac <dac@192.168.1.8>
2014-09-14 12:51:12 +0200

    Add sequence handler.

---
+++ src/SequenceHandler.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Kata;
+
+class SequenceHandler
+{
+	public function getMinimumValue($sequence)
+	{
+		return -2;
+	}
+
+	public function getMaximumValue($sequence)
+	{
+		return 92;
+	}
+
+	public function getNumberOfElements($sequence)
+	{
+		return 6;
+	}
+
+	public function getAverage($sequence)
+	{
+		return 21.8333;
+	}
+
+}
---
+++ test/Kata00/SequenceHandlerTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Kata\Test\SequenceHandler;
+
+use Kata\SequenceHandler;
+
+class SequenceHandlerTest extends \PHPUnit_Framework_TestCase
+{
+	const MIN_VALUE = 0;
+	const MAX_VALUE = 1;
+	const NUM_VALUE = 2;
+	const AVG_VALUE = 3;
+
+	/**
+	 * @dataProvider providerSequence
+	 */
+	public function testMinimumValue($result, $sequence)
+	{
+		$sequenceHandler = new SequenceHandler();
+
+		$this->assertEquals($result[self::MIN_VALUE], $sequenceHandler->getMinimumValue($sequence));
+	}
+
+	/**
+	 * @dataProvider providerSequence
+	 */
+	public function testMaximumValue($result, $sequence)
+	{
+		$sequenceHandler = new SequenceHandler();
+
+		$this->assertEquals($result[self::MAX_VALUE], $sequenceHandler->getMaximumValue($sequence));
+	}
+
+	/**
+	 * @dataProvider providerSequence
+	 */
+	public function testNumberOfElements($result, $sequence)
+	{
+		$sequenceHandler = new SequenceHandler();
+
+		$this->assertEquals($result[self::NUM_VALUE], $sequenceHandler->getNumberOfElements($sequence));
+	}
+
+	/**
+	 * @dataProvider providerSequence
+	 */
+	public function testAverageValue($result, $sequence)
+	{
+		$sequenceHandler = new SequenceHandler();
+
+		$this->assertEquals($result[self::AVG_VALUE], $sequenceHandler->getAverage($sequence));
+	}
+
+	public function providerSequence()
+	{
+		return array(
+			array(array(-2, 92, 6, 21.8333), array(6, 9, 15, -2, 92, 11)),
+		);
+	}
+
+}

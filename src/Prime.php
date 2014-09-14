ad27e5aed461571ffba8f3b85c41332de64e57cc
dac <dac@192.168.1.8>
2014-09-14 12:23:51 +0200

    Test by provider.

--- src/Prime.php
+++ src/Prime.php
@@ -4,20 +4,34 @@ namespace Kata;
 
 class Prime
 {
-	public function getPrimes($number)
+	/**
+	 * Returns number's prime decomposition.
+	 *
+	 * @param int $number
+	 *
+	 * @return array
+	 */
+	public function getPrimeDecomposition($number)
 	{
 		$primes = array();
+
+		if ($number < 0)
+		{
+			$primes[] = -1;
+			$number   = $number * (-1);
+		}
 
 		for ($i = 2; $i <= $number; $i++)
 		{
 			if ($number % $i === 0)
 			{
 				$primes[] = $i;
-				$number /= $i;
-				$i--;
+				$number  /= $i;
+				$i       -= 1;
 			}
 		}
 
 		return $primes;
 	}
+
 }
--- test/PrimeTest.php
+++ test/PrimeTest.php
@@ -6,17 +6,45 @@ use Kata\Prime;
 
 class PrimeTest extends \PHPUnit_Framework_TestCase
 {
-	public function testPrime()
+	/**
+	 * @dataProvider providerPrimeDecomposition
+	 */
+	public function testPrimeDecomposition($number, $decomposition)
 	{
 		$prime = new Prime();
 
-		$this->assertEquals(array(2),       $prime->getPrimes(2));
-		$this->assertEquals(array(3),       $prime->getPrimes(3));
-		$this->assertEquals(array(2, 2),    $prime->getPrimes(4));
-//		$this->assertEquals(array(2, 3),    $prime->getPrimes(6));
-//		$this->assertEquals(array(3, 3),    $prime->getPrimes(9));
-//		$this->assertEquals(array(2, 2, 3), $prime->getPrimes(12));
-//		$this->assertEquals(array(3, 5),    $prime->getPrimes(15));
+		$this->assertEquals($decomposition, $prime->getPrimeDecomposition($number));
 	}
-}
 
+	public function providerPrimeDecomposition()
+	{
+		return array(
+			// Negative cases.
+			array(-4, array(-1, 2, 2)),
+			array(-2, array(-1, 2)),
+
+			// Extreme cases.
+			array(0,  array()),
+			array(1,  array()),
+
+			// Textbook examples.
+			array(2,  array(2)),
+			array(3,  array(3)),
+			array(4,  array(2, 2)),
+			array(6,  array(2, 3)),
+			array(9,  array(3, 3)),
+			array(12, array(2, 2, 3)),
+			array(15, array(3, 5)),
+
+			// Fermat's factorization: 2^2*n + 1
+			array(5,     array(5)),
+			array(17,    array(17)),
+			array(257,   array(257)),
+			array(65537, array(65537)),
+
+			// Wiki's example.
+			array(9438, array(2, 3, 11, 11, 13)),
+		);
+	}
+
+}
